09c0b1ce76e7d18fdf4be13c4676d58f19f6bbf7
devstackmindmap <devstackmindmap@gamil.com>
2022-12-05 01:15:25 +0900

    linq projection operators

--- charp/0002.LinqRestriction.cs
+++ charp/0002.LinqRestriction.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-public class Linq_Restriction
+public class LinqRestriction
 {
     public static void Run()
     {
@@ -18,7 +18,7 @@ public class Linq_Restriction
         }
 
         // Filter elements on a property
-        List<Product> products = GetProductList();
+        List<Product> products = ProductHelper.GetProductList();
 
         var soldOutProducts = from prod in products
                             where prod.UnitsInStock == 0
@@ -31,7 +31,7 @@ public class Linq_Restriction
         }
 
         // Filter elements on multiple properties
-        products = GetProductList();
+        products = ProductHelper.GetProductList();
 
         var expensiveInStockProducts = from prod in products
                                     where prod.UnitsInStock > 0 && prod.UnitPrice > 3.00
@@ -44,7 +44,7 @@ public class Linq_Restriction
         }
 
         // Examine a sequence property of output elements
-        List<Customer> customers = GetCustomerList();
+        List<Customer> customers = ProductHelper.GetCustomerList();
 
         var waCustomers = from cust in customers
                         where cust.Region == "WA"
@@ -70,49 +70,7 @@ public class Linq_Restriction
         {
             Console.WriteLine($"The word {d} is shorter than its value.");
         }
-    }
-
-    private static List<Product> GetProductList()
-    {
-        return new List<Product>{
-            new Product { ProductName = "A", UnitsInStock = 0, UnitPrice = 3.1d },
-            new Product { ProductName = "B", UnitsInStock = 1, UnitPrice = 3.0d},
-            new Product { ProductName = "C", UnitsInStock = 1, UnitPrice = 3.1d},
-            new Product { ProductName = "D", UnitsInStock = 0, UnitPrice = 3.0d}
-        };
-    }
-
-    private static List<Customer> GetCustomerList()
-    {
-        return new List<Customer>{
-            new Customer { Region = "WA", CustomerID = 1, CompanyName = "A Company", Orders = {new Order { OrderID = 1, OrderDate = "1"}}},
-            new Customer { Region = "WA", CustomerID = 2, CompanyName = "B Company", Orders = {new Order { OrderID = 1, OrderDate = "1"}}},
-            new Customer { Region = "KR", CustomerID = 3, CompanyName = "A Company", Orders = {new Order { OrderID = 1, OrderDate = "1"}}},
-            new Customer { Region = "KR", CustomerID = 4, CompanyName = "B Company", Orders = {new Order { OrderID = 1, OrderDate = "1"}}},
-        };
-    }
-
-    
+    }    
 }
 
-class Product
-{
-    public string ProductName = "";
-    public int UnitsInStock = 0;
 
-    public double UnitPrice = 0d;
-}
-
-class Customer
-{
-    public string Region = "";
-    public int CustomerID = 0;
-    public string CompanyName = "";
-    public List<Order> Orders = new List<Order>();
-}
-
-class Order
-{
-    public int OrderID;
-    public string OrderDate = "1";
-}
---
+++ charp/0003.Projection_Operators.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+public class ProjectionOperators
+{
+    public static void Run()
+    {
+        // Select clause
+        int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+        var numsPlusOne = from n in numbers
+                        select n + 1;
+
+        Console.WriteLine("Numbers + 1:");
+        foreach (var i in numsPlusOne)
+        {
+            Console.WriteLine(i);
+        }
+
+        // Select a single property
+        List<Product> products = ProductHelper.GetProductList();
+
+        var productNames = from p in products
+                   select p.ProductName;
+
+        Console.WriteLine("Product Names:");
+        foreach (var productName in productNames)
+        {
+            Console.WriteLine(productName);
+        }
+
+        // Transform with select
+        string[] strings = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        var textNums = from n in numbers
+                    select strings[n];
+
+        Console.WriteLine("Number strings:");
+        foreach (var s in textNums)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
