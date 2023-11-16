9a6f8e0d9b1617074b5e08cbd3edf70ae9dd58d4
devstackmindmap <devstackmindmap@gamil.com>
2023-11-16 09:55:31 +0900

    Serilog Test

---
+++ csharp/LoggerSeri/Player.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerSeri
+{
+    internal class Player
+    {
+        public Player() { }
+
+        public void Attack() 
+        {
+            Log.Information("Attack");
+        }
+    }
+}
---
+++ csharp/LoggerSeri/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LoggerSeri;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Slack.Models;
+using Serilog.Sinks.Slack;
+using Serilog.Core;
+
+// Todo : 구조화된 로깅, 로그 비동기화 
+// Log Level 수정
+
+var levelSwitch = new LoggingLevelSwitch();
+
+// 동적 레벨
+
+levelSwitch.MinimumLevel = LogEventLevel.Debug;
+Log.Logger = new LoggerConfiguration()
+    //.MinimumLevel.Debug()
+    .MinimumLevel.ControlledBy(levelSwitch)
+    .WriteTo.Console()
+    .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day)
+    //.WriteTo.File("log.txt",
+    //    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .WriteTo.Slack(new SlackSinkOptions
+    {
+        WebHookUrl = "https://hooks.slack.com/services/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",
+        BatchSizeLimit = 20,
+        QueueLimit = 1000,
+        CustomUserName = "Slack Logger",
+        CustomIcon = ":ghost:",
+        Period = TimeSpan.FromSeconds(10),
+        ShowDefaultAttachments = false,
+        ShowExceptionAttachments = true,
+        MinimumLogEventLevel = LogEventLevel.Debug,
+        PropertyDenyList = new List<string> { "Level", "SourceContext" }
+    })
+    .CreateLogger();
+
+Log.Information("INFO Hello");
+Log.Warning("WAR Hello");
+
+levelSwitch.MinimumLevel = LogEventLevel.Verbose;
+
+Log.Information("INFO Hello");
+Log.Warning("WAR Hello");
+
+var player = new Player();
+player.Attack();
+
+Log.CloseAndFlush();
+
+
+class 
