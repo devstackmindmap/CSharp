ec8e003fe5ad5fda5d0cc9c20f436fda93d0226d
devstackmindmap <devstackmindmap@gamil.com>
2023-11-27 11:47:02 +0900

    Model Validation, Data Annotations

---
+++ ModelValidationDemo/ModelValidation.API/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidation.API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ModelValidation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // POST api/<UsersController>
+        [HttpPost]
+        public ActionResult<User> Post([FromBody] User user)
+        {
+            //Some Operations
+            return Ok(user);
+        }
+    }
+}
