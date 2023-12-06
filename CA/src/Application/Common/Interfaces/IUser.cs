2a710eb144d5fe86d2a6559d4c770302a6bf2a96
dykim <dykim@ctrl9.co.kr>
2023-12-06 16:52:33 +0900

    CA Init

---
+++ CA/src/Application/Common/Interfaces/IUser.cs
@@ -0,0 +1,6 @@
+﻿namespace CA.Application.Common.Interfaces;
+
+public interface IUser
+{
+    string? Id { get; }
+}
---
+++ CA/src/Web/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Ardalis.GuardClauses;
+global using CA.Web.Infrastructure;
+global using MediatR;
